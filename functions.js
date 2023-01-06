6a54cb52da8ea19a7cf7f821d0ef84b1f43e1ed6
locust580 <hunterbearintx@gmail.com>
2023-01-05 23:26:28 -0600

    created new system for item creation: itemadd.html now also shows items and will be pass protected index.html grabs from itemadd items to update the ones shown on the homepage cheers

--- functions.js
+++ functions.js
@@ -4,21 +4,15 @@ var percentTop = 25;
 var itemCount = 0;
 var idForItem = "";
 
-/*function buttonpress(button, url) {
-	onclick(button, )
-}
-*/
-
 function itemadd(){
-	const item = document.createElement('div');
+	const item = document.getElementById("itemClump").appendChild(document.createElement('div'));
 	item.id = 'item' + itemcount;
 	idForItem = item.id
-	document.getElementById("body").appendChild(item);
 	document.getElementById(idForItem).style.backgroundColor = "grey";
 	document.getElementById(idForItem).style.width = "15%";
 	document.getElementById(idForItem).style.height = "15%";
 	document.getElementById(idForItem).style.position = "absolute";
-		if (percentleft != 77){
+		if (percentleft !== 77){
 			document.getElementById(idForItem).style.left = percentLeft + "%";
 			document.getElementById(idForItem).style.top = percentTop + "%";
 			percentleft += 23
