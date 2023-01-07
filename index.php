df0d8a21c75616de5ac9f308af62fd2cb26c9aca
locust580 <hunterbearintx@gmail.com>
2023-01-06 19:33:52 -0600

    added php code to connect to web server

--- index.php
+++ index.php
@@ -17,6 +17,22 @@
     <title>Westlake Academy Lost and Found</title>
   </head>
   <body onload="itemGrab">
+  <?php
+  $host_name = 'db5011453042.hosting-data.io';
+  $database = 'dbs9662413';
+  $user_name = 'dbu2115371';
+  $password = '<Enter your password here.>';
+
+  $link = new mysqli($host_name, $user_name, $password, $database);
+
+  if ($link->connect_error) {
+    die('<p>Failed to connect to MySQL: '. $link->connect_error .'</p>');
+  } else {
+    echo '<p>Connection to MySQL server successfully established.</p>';
+  }
+?>
+
+
     <div class="topnav">
   <a class="active" href="#home">Home</a>
   <a href="about.php">About</a>
