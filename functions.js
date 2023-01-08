6e5b6abf98a2fdcf4de7caf3cb6542c3f26009dc
locust580 <hunterbearintx@gmail.com>
2023-01-08 17:55:19 -0600

    Major item list update

--- functions.js
+++ functions.js
@@ -31,9 +31,13 @@ function itemadd(){
 	const itemBox = document.body.appendChild(document.createElement("div"));
 	itemBox.className = "itemBox";
 	itemBox.id = itemCount;
-	const previewImg = itemBox.appendChild(document.createElement("img"));
+	const figure = itemBox.appendChild(document.createElement("figure"));
+	figure.className = "figure"
+	const previewImg = figure.appendChild(document.createElement("img"));
 	previewImg.className = "itemPreview";
 	previewImg.id = "previewImg" + itemCount
+	const caption = figure.appendChild(document.createElement("figcaption"))
+	caption.id = "caption" + itemCount
 	const formInputLabel = itemBox.appendChild(document.createElement("label"));
 	formInputLabel.innerHTML  = "Upload Image";
 	formInputLabel.className = "formInputLabel"
@@ -59,11 +63,26 @@ function itemadd(){
 
 function showPreview(event, itemNum){
 	if(event.target.files.length > 0){
-	  var idForImg = "previewImg" + itemNum
-	  var src = URL.createObjectURL(event.target.files[0]);
-	  var preview = document.getElementById(idForImg);
-	  preview.src = src;
-	  preview.style.display = "block";
+		var idForImg = "previewImg" + itemNum
+		var src = URL.createObjectURL(event.target.files[0]);
+		var preview = document.getElementById(idForImg);
+		preview.src = src; 
+		preview.style.display = "block";
+		preview.style.height = "auto";
+		preview.style.width = "60%";
+		preview.style.objectFit = "contain";
+
 	}
 }
 
+/*
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 200;
+canvas.height = 150;
+
+ctx.drawImage(document.getElementById(idForImg), 0, 0, canvas.width, canvas.height);
+const dataURI = canvas.toDataURL();
+preview.src = dataURI
+
+*/
