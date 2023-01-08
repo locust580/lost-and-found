29d45595c03bdf2112b84e2244e11ed6ac35fc0a
locust580 <hunterbearintx@gmail.com>
2023-01-07 20:28:23 -0600

    Tutorial help Used a tutorial, will edit code later to mesh with mine

--- functions.js
+++ functions.js
@@ -3,6 +3,26 @@ var percentLeft = 8;
 var percentTop = 25;
 var itemCount = 0;
 var idForItem = "";
+const input = document.querySelector("input")
+const output = document.querySelector("output")
+let imagesArray = []
+
+input.addEventListener("change", () => {
+	const file = input.files
+	imagesArray.push(file[0])
+	displayImages()
+  })
+
+function displayImages() {
+  let images = ""
+  imagesArray.forEach((image, index) => {
+    images += `<div class="image">
+                <img src="${URL.createObjectURL(image)}" alt="image">
+                <span onclick="deleteImage(${index})">&times;</span>
+              </div>`
+  })
+  output.innerHTML = images
+}
 
 function itemadd(){
 	const item = document.getElementById("itemClump").appendChild(document.createElement('div'));
