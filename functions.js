cf420790bacba6b4a25e641ac265f17e2973b327
locust580 <hunterbearintx@gmail.com>
2023-01-08 12:18:33 -0600

    New itemadd system

--- functions.js
+++ functions.js
@@ -28,7 +28,42 @@ var idForItem = "";
 */
 
 function itemadd(){
-	const item = document.getElementById("itemclump")	
-  		.appendChild(document.createElement('div'))
-  		.appendChild(document.createElement('div'))
+	const itemBox = document.body.appendChild(document.createElement("div"));
+	itemBox.className = "itemBox";
+	itemBox.id = itemCount;
+	const previewImg = itemBox.appendChild(document.createElement("img"));
+	previewImg.className = "itemPreview";
+	previewImg.id = "itemPreview" + itemCount
+	const formInputLabel = itemBox.appendChild(document.createElement("label"));
+	formInputLabel.innerHTML  = "Upload Image";
+	formInputLabel.className = "formInputLabel"
+	const formInput = formInputLabel.appendChild(document.createElement("input"));
+	formInput.className = "form-input";
+	idForItem = itemBox.id
+	formInput.setAttribute("type", "file")
+	formInput.setAttribute("accept", "image/*")
+	formInput.setAttribute("onchange", `showPreview(event, ${itemCount});`)
+		if (percentLeft !== 100){
+			document.getElementById(idForItem).style.left = percentLeft + "%";
+			document.getElementById(idForItem).style.top = percentTop + "%";
+			percentLeft += 23
+		} else {
+			percentLeft = 8
+			percentTop += 25
+			document.getElementById(idForItem).style.left = percentLeft + "%";
+			document.getElementById(idForItem).style.top = percentTop + "%";
+			percentLeft += 23
+		}
+	itemCount +=1
 }
+
+function showPreview(event, itemNum){
+	if(event.target.files.length > 0){
+	  var idForImg = "previewImg" + itemNum
+	  var src = URL.createObjectURL(event.target.files[0]);
+	  var preview = document.getElementById(idForImg);
+	  preview.src = src;
+	  preview.style.display = "block";
+	}
+}
+
